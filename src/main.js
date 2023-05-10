06d33af7d7c668ee3c81e9c08edccbfd15decaa1
Reny Reyes <98413865+Reny8@users.noreply.github.com>
2023-05-10 15:20:08 -0400

    Move sample into a pages folder

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-import Sample from './components/Sample.vue';
+import Sample from './pages/Sample.vue';
 
 const router = createRouter({
   history: createWebHistory(),
